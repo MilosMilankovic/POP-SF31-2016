edfb29d1bea7d9f43a9f5e6d61f3ee2b7b385b95
Milos Milankovic <milankovicmilos@gmail.com>
2017-11-26 12:56:45 +0100

    Vidljivost TipNamestaja kod dodavanja i izmene.

---
+++ Novi projekat GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using POP_31.Model;
+using POP_31.Util;
+using POP31_MILOS_GUI.Windows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POP31_MILOS_GUI
+{
+ // da izbaci tip namestaja kad se otvori izmjena i dodavanje
+ // da izmene upise u xml
+
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Namestaj> lista;
+        public static ObservableCollection<TipNamestaja> tipNamLista;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            lista = new ObservableCollection<Namestaj>();
+            lista= Projekat.Instance.Namestaj;
+            tipNamLista = new ObservableCollection<TipNamestaja>();
+            tipNamLista = GenericSerializer.Deserialize<TipNamestaja>("tipoviNamestaja.xml");
+            dataGr.ItemsSource = lista;
+
+
+
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            AddWindow window = new AddWindow();
+            window.labelaDodavanje.IsEnabled = true;
+            window.labelaIzmena.IsEnabled = false;
+            window.ShowDialog();
+            dataGr.Items.Refresh();
+        }
+        private void Izmeni(object sender, RoutedEventArgs e)
+        {
+           
+            if(dataGr.SelectedItem!=null)
+            {
+                AddWindow prozor = new AddWindow(dataGr.SelectedItem as Namestaj);
+                prozor.labelaDodavanje.IsEnabled = false;
+                prozor.labelaIzmena.IsEnabled = true;
+                prozor.ShowDialog();
+                //dataGr.Items.Refresh();
+            }
+        }
+        private void Obrisi(object sender, RoutedEventArgs e)
+        {
+            if (dataGr.SelectedItem != null)
+            {
+                if (MessageBox.Show($"Da li ste sigurni da zelite da izbrisete?", "Brisanje", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    Namestaj n = dataGr.SelectedItem as Namestaj;
+                    foreach (Namestaj nam in MainWindow.lista)
+                    {
+                        if (nam.Id == n.Id)
+                        {
+                            MainWindow.lista.Remove(nam);
+                            break;
+                        }
+                    }
+                    dataGr.Items.Refresh();
+                    GenericSerializer.Serialize<Namestaj>("Namestaj.xml", lista);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Niste selektovali sta hocete da obrisete!");
+                
+            }
+        }
+
+    }
+}
---
+++ Novi projekat GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/Model/Namestaj.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static POP_31.Model.Namestaj;
+
+namespace POP_31.Model
+{
+    [Serializable]
+
+    public class Namestaj : INotifyPropertyChanged
+    {
+        private int id;
+        private string naziv;
+        private double jedinicnaCena;
+        private int kolicinaUmagacinu;
+        private string sifra;
+        private TipNamestaja tipNamestaja;
+        private int tipNamestajaId;
+        private bool obrisan;
+
+        public bool Obrisan
+        {
+            get { return obrisan; }
+            set
+            {
+                obrisan = value;
+                OnPropertyChanged("Obrisan");
+            }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Naziv
+        {
+            get { return naziv; }
+            set
+            {
+                naziv = value;
+                OnPropertyChanged("Naziv");
+            }
+        }
+        public double JedinicnaCena
+        {
+            get { return jedinicnaCena; }
+            set
+            {
+                jedinicnaCena = value;
+                OnPropertyChanged("JedinicnaCena");
+            }
+        }
+
+        public int KolicinaUmagacinu
+        {
+            get { return kolicinaUmagacinu; }
+            set
+            {
+                kolicinaUmagacinu = value;
+                OnPropertyChanged("KolicinaUmagacinu");
+            }
+        }
+
+
+        public string Sifra
+        {
+            get { return sifra; }
+            set
+            {
+                sifra = value;
+                OnPropertyChanged("Sifra");
+            }
+        }
+
+        //[XmlIgnore] 
+        public TipNamestaja TipNamestaja
+        {
+            get
+            {
+                if(tipNamestaja ==null)
+                {
+                    return null;
+                }
+                return tipNamestaja;
+            }
+            set
+            {
+                tipNamestaja = value;
+                OnPropertyChanged("TipNamestaja");
+
+            }
+        }
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+
+   
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null) 
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
