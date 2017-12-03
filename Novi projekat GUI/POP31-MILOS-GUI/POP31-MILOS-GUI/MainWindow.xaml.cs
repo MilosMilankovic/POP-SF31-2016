d20406cd199ee87da4dee77b285141321f8aa59d
Milos Milankovic <milankovicmilos@gmail.com>
2017-12-03 20:51:45 +0100

    Napravljen racun, aut generisanje kolona.

---
+++ Novi projekat GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using POP_31.Model;
+using POP_31.Util;
+using POP31_MILOS_GUI.Windows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POP31_MILOS_GUI
+{
+ // da izbaci tip namestaja kad se otvori izmjena i dodavanje
+ // da izmene upise u xml
+
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Namestaj> lista;
+        public static ObservableCollection<TipNamestaja> tipNamLista;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            lista = new ObservableCollection<Namestaj>();
+            lista= Projekat.Instance.Namestaj;
+            tipNamLista = new ObservableCollection<TipNamestaja>();
+            tipNamLista = GenericSerializer.Deserialize<TipNamestaja>("tipoviNamestaja.xml");
+            dataGr.ItemsSource = lista;
+
+
+
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            AddWindow window = new AddWindow();
+            window.labelaIzmena.Content = "Dodavanje";
+            window.ShowDialog();
+            dataGr.Items.Refresh();
+        }
+        private void Izmeni(object sender, RoutedEventArgs e)
+        {
+           
+            if(dataGr.SelectedItem!=null)
+            {
+                AddWindow prozor = new AddWindow(dataGr.SelectedItem as Namestaj);
+                prozor.labelaIzmena.Content = "Izmena";
+                prozor.ShowDialog();
+                dataGr.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Morate odabrati sta menjate!");
+            }
+        }
+        private void Obrisi(object sender, RoutedEventArgs e)
+        {
+            if (dataGr.SelectedItem != null)
+            {
+                if (MessageBox.Show($"Da li ste sigurni da zelite da izbrisete?", "Brisanje", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    Namestaj n = dataGr.SelectedItem as Namestaj;
+                    foreach (Namestaj nam in MainWindow.lista)
+                    {
+                        if (nam.Id == n.Id)
+                        {
+                            MainWindow.lista.Remove(nam);
+                            break;
+                        }
+                    }
+                    dataGr.Items.Refresh();
+                    GenericSerializer.Serialize<Namestaj>("Namestaj.xml", lista);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Niste selektovali sta hocete da obrisete!");
+                
+            }
+        }
+
+        private void Prikaz_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if ((string)e.Column.Header =="Id" ) 
+            {
+                e.Cancel = true;
+            }
+
+            if ((string)e.Column.Header == "Obrisan")
+            {
+                e.Cancel = true;
+            }
+
+            if ((string)e.Column.Header == "TipNamestaja")
+            {
+                e.Cancel = true;
+            }
+
+
+        }
+
+        private void Racun(object sender, RoutedEventArgs e)
+        {
+            if(dataGr.SelectedItem!=null)
+            {
+                TicketWindow win = new TicketWindow(dataGr.SelectedItem as Namestaj);
+                win.ShowDialog();
+                GenericSerializer.Serialize<Namestaj>("Namestaj.xml", lista);
+            }
+            else
+            {
+                MessageBox.Show("Morate selektovati sta kupujete");
+            }
+        }
+
+        private void Zatvori(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Novi projekat GUI/POP31-MILOS-GUI/POP31-MILOS-GUI/Windows/TicketWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using POP_31.Model;
+using POP_31.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POP31_MILOS_GUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for TicketWindow.xaml
+    /// </summary>
+    public partial class TicketWindow : Window
+    {
+        List<DodatnaUsluga> usluge;
+        private Namestaj nam;
+        public TicketWindow()
+        {
+            InitializeComponent();
+            usluge = new List<DodatnaUsluga>();
+        }
+
+        public TicketWindow(Namestaj n)
+        {
+            nam = n;
+            InitializeComponent();
+            usluge = new List<DodatnaUsluga>();
+            DodatnaUsluga d1 = new DodatnaUsluga();
+            d1.Naziv = "Prevoz";
+            d1.Cena = 3000;
+
+            DodatnaUsluga d2 = new DodatnaUsluga();
+            d2.Naziv = "Montaza";
+            d2.Cena = 5000;
+
+            usluge.Add(d1);
+            usluge.Add(d2);
+            comboBoxDU.ItemsSource = usluge;
+
+            DataContext = n; //binding path ce traziti taj properti u instanci klase n
+        }
+
+        private void Finish(object sender, RoutedEventArgs e)
+        {
+            double ukupnaCena = 0;
+                DodatnaUsluga d = comboBoxDU.SelectedItem as DodatnaUsluga;
+            if (comboBoxDU.SelectedItem != null)
+            {
+                ukupnaCena = nam.JedinicnaCena + d.Cena;
+            }
+            else
+            {
+                ukupnaCena = nam.JedinicnaCena;
+            }
+                if (MessageBox.Show($"Ukupan racun iznosi {ukupnaCena},da li zelite da kupite?", "Racun", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    MessageBox.Show("Uspesno obavljena kupovina");
+                    foreach(Namestaj a in MainWindow.lista)
+                    {
+                        if(a.Id==nam.Id)
+                        {
+                            a.KolicinaUmagacinu--;
+                        break;
+                        }
+                    }
+                this.Close();
+
+                }
+            this.Close();
+            
+        }
+    }
+}
